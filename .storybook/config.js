1ba69e39a23eeb62a78a8e2b4b1483a09b66a1c8
Artem Samofalov <dex157@gmail.com>
2018-06-14 01:06:21 +0500

    add theme provider

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,7 @@
-import { configure } from '@storybook/react'
+import React from 'react'
+import { configure, addDecorator } from '@storybook/react'
+import theme from '../src/theme'
+import { ThemeProvider } from 'styled-components'
 import '../src/index.css'
 
 const req = require.context('../src', true, /\.stories\.js$/)
@@ -6,5 +9,5 @@ const req = require.context('../src', true, /\.stories\.js$/)
 function loadStories() {
   req.keys().forEach(filename => req(filename))
 }
-
+addDecorator(story => <ThemeProvider theme={theme}>{story()}</ThemeProvider>)
 configure(loadStories, module)
